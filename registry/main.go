2db64f51e0c6ae844ef94ef172399c1fee8e8d5c
Greyson Sequino <gcsequino@gmail.com>
2024-05-13 12:01:54 -0600

    async endpoint

--- registry/main.go
+++ registry/main.go
@@ -6,6 +6,8 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"sync"
+	"time"
 )
 
 func getRoot(w http.ResponseWriter, r *http.Request) {
@@ -34,6 +36,44 @@ func main() {
 		fmt.Printf("server closed\n")
 	} else if err != nil {
 		fmt.Printf("error starting server: %s\n", err)
+}
+
+func postName(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "Invalid request method", http.StatusBadRequest)
+		return
+	}
+	name := r.FormValue("name")
+	fmt.Printf("Received name: %s\n", name)
+	io.WriteString(w, "Hello, "+name+"!")
+}
+
+func longRunningTask(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("Starting long running task...\n")
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		time.Sleep(5 * time.Second)
+		fmt.Printf("Long running task completed!\n")
+		io.WriteString(w, "Long running task completed!\n")
+	}()
+	wg.Wait()
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", getRoot)
+	mux.HandleFunc("/hello", getHello)
+	mux.HandleFunc("/bye", getBye)
+	mux.HandleFunc("/postName", postName)
+	mux.HandleFunc("/longRunningTask", longRunningTask)
+
+	err := http.ListenAndServe(":3333", mux)
+	if errors.Is(err, http.ErrServerClosed) {
+		fmt.Printf("server closed\n")
+	} else if err != nil {
+		fmt.Printf("error starting the server: %s\n", err)
 		os.Exit(1)
 	}
 }
