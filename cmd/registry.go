c5bc3205674aa1744153eb2164c84aa265ace842
Greyson Sequino <gcsequino@gmail.com>
2024-05-24 18:29:04 -0600

    initial cobra setup complete

---
+++ cmd/registry.go
@@ -0,0 +1,42 @@
+/*
+Copyright © 2024 Greyson Seuqino
+
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/gcsequino/GoOrch/internal/registry"
+	"github.com/spf13/cobra"
+)
+
+// registryCmd represents the registry command
+var registryCmd = &cobra.Command{
+	Use:   "registry",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("registry called")
+		registry.Hello()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(registryCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// registryCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// registryCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2024 Greyson Sequino <gcsequino@gmail.com>
+
+*/
+package main
+
+import "github.com/gcsequino/GoOrch/cmd"
+
+func main() {
+	cmd.Execute()
+}
